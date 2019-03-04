70b7440f1c9635be7e17af56c8fd4196bec36f67
Jacques Chester <jchester@pivotal.io>
2019-03-04 16:18:16 -0500

    Initial commit, ugly as sin

---
+++ go.mod
@@ -0,0 +1,41 @@
+module knative-simulator
+
+go 1.12
+
+require (
+	github.com/evanphx/json-patch v4.1.0+incompatible // indirect
+	github.com/golang/groupcache v0.0.0-20190129154638-5b532d6fd5ef // indirect
+	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c // indirect
+	github.com/google/go-containerregistry v0.0.0-20190222233527-d3e6a441f49f // indirect
+	github.com/google/gofuzz v0.0.0-20170612174753-24818f796faf // indirect
+	github.com/googleapis/gnostic v0.2.0 // indirect
+	github.com/gregjones/httpcache v0.0.0-20190212212710-3befbb6ad0cc // indirect
+	github.com/hashicorp/golang-lru v0.5.0 // indirect
+	github.com/json-iterator/go v1.1.5 // indirect
+	github.com/knative/build v0.4.0 // indirect
+	github.com/knative/caching v0.0.0-20190118010831-97110f4e0cee // indirect
+	github.com/knative/pkg v0.0.0-20190225184048-41761b2c813c // indirect
+	github.com/knative/serving v0.4.0
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/mattbaird/jsonpatch v0.0.0-20171005235357-81af80346b1a // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/onsi/ginkgo v1.7.0 // indirect
+	github.com/onsi/gomega v1.4.3 // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/prometheus/common v0.2.0
+	github.com/spf13/pflag v1.0.3 // indirect
+	go.opencensus.io v0.19.0 // indirect
+	go.uber.org/atomic v1.3.2 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.9.1
+	golang.org/x/oauth2 v0.0.0-20190226191147-529b322ea346 // indirect
+	golang.org/x/time v0.0.0-20181108054448-85acf8d2951c // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	k8s.io/api v0.0.0-20190202010724-74b699b93c15 // indirect
+	k8s.io/apimachinery v0.0.0-20190117220443-572dfc7bdfcb // indirect
+	k8s.io/client-go v10.0.0+incompatible
+	k8s.io/klog v0.2.0 // indirect
+	k8s.io/kube-openapi v0.0.0-20190225204428-d50a959ae76a // indirect
+	sigs.k8s.io/yaml v1.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,205 @@
+package main
+
+import (
+	"context"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/knative/serving/pkg/autoscaler"
+	"github.com/prometheus/common/log"
+	"go.uber.org/zap"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/informers/core/v1"
+	"k8s.io/client-go/kubernetes"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	fakes "k8s.io/client-go/kubernetes/fake"
+)
+
+const (
+	stableWindow           = 60 * time.Second
+	panicWindow            = 6 * time.Second
+	scaleToZeroGracePeriod = 30 * time.Second
+	testNamespace = "simulator-namespace"
+)
+
+var (
+	informerFactory   informers.SharedInformerFactory
+	endpointsInformer v1.EndpointsInformer
+	fakeClient        kubernetes.Interface
+)
+
+func main() {
+	fmt.Println("starting")
+
+	unsugaredLogger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatal("config error!!1!: %s", err.Error())
+	}
+	logger := unsugaredLogger.Sugar()
+
+	fakeClient = fakes.NewSimpleClientset()
+	informerFactory = informers.NewSharedInformerFactory(fakeClient, 0)
+	endpointsInformer = informerFactory.Core().V1().Endpoints()
+
+	config := &autoscaler.Config{
+		ContainerConcurrencyTargetPercentage: 1.0, // targeting 100% makes the test easier to read
+		ContainerConcurrencyTargetDefault:    15.0,
+		MaxScaleUpRate:                       100.0,
+		StableWindow:                         stableWindow,
+		PanicWindow:                          panicWindow,
+		ScaleToZeroGracePeriod:               scaleToZeroGracePeriod,
+	}
+
+	dynConfig := autoscaler.NewDynamicConfig(config, logger)
+
+	as, err := autoscaler.New(
+		dynConfig,
+		testNamespace,
+		"revisionService",
+		endpointsInformer,
+		100.0,
+		&mockReporter{},
+	)
+	ctx := context.TODO()
+
+	t := time.Now()
+
+	w := csv.NewWriter(os.Stdout)
+
+	err = w.Write([]string{"time", "avg_concurrent_requests", "request_count", "desired_replicas"})
+	if err != nil {
+		logger.Fatal("could not write header: %s", err.Error())
+	}
+
+	var stepper SimStepper
+	stepper = &linear{step: 0}
+
+	for i := int32(0); i < 200; i++ {
+		stepper.Step(int(i))
+
+		t = t.Add(time.Second)
+		avgConcurrent := float64(stepper.AverageConcurrent())
+		//reqCount := i
+
+		stat := autoscaler.Stat{
+			Time:                      &t,
+			PodName:                   fmt.Sprintf("simulator-pod-%d", i),
+			AverageConcurrentRequests: avgConcurrent,
+			//RequestCount:              reqCount,
+			LameDuck:                  false,
+		}
+		as.Record(ctx, stat)
+		desired, _ := as.Scale(ctx, t)
+
+		createEndpoints(addIps(makeEndpoints(), int(stepper.RunningPods())))
+
+		err = w.Write([]string{
+			strconv.Itoa(int(t.Unix())),
+			strconv.FormatFloat(avgConcurrent, 'f', 2, 64),
+			//strconv.Itoa(int(reqCount)),
+			strconv.Itoa(int(desired)),
+		})
+
+		if err != nil {
+			logger.Fatal("could not write record: %s", err.Error())
+		}
+	}
+	w.Flush()
+}
+
+type SimStepper interface {
+	AverageConcurrent() float64
+	RunningPods() int
+	Step(step int)
+}
+
+type linear struct {
+	step int
+}
+
+func (l *linear) AverageConcurrent() float64 {
+	return float64(l.step)
+}
+
+func (l *linear) RunningPods() int {
+	return l.step
+}
+
+func (l *linear) Step(step int) {
+	if step < 50 {
+		l.step = step
+	}
+}
+
+type mockReporter struct{}
+
+// ReportDesiredPodCount of a mockReporter does nothing and return nil for error.
+func (r *mockReporter) ReportDesiredPodCount(v int64) error {
+	return nil
+}
+
+// ReportRequestedPodCount of a mockReporter does nothing and return nil for error.
+func (r *mockReporter) ReportRequestedPodCount(v int64) error {
+	return nil
+}
+
+// ReportActualPodCount of a mockReporter does nothing and return nil for error.
+func (r *mockReporter) ReportActualPodCount(v int64) error {
+	return nil
+}
+
+// ReportObservedPodCount of a mockReporter does nothing and return nil for error.
+func (r *mockReporter) ReportObservedPodCount(v float64) error {
+	return nil
+}
+
+// ReportStableRequestConcurrency of a mockReporter does nothing and return nil for error.
+func (r *mockReporter) ReportStableRequestConcurrency(v float64) error {
+	return nil
+}
+
+// ReportPanicRequestConcurrency of a mockReporter does nothing and return nil for error.
+func (r *mockReporter) ReportPanicRequestConcurrency(v float64) error {
+	return nil
+}
+
+// ReportTargetRequestConcurrency of a mockReporter does nothing and return nil for error.
+func (r *mockReporter) ReportTargetRequestConcurrency(v float64) error {
+	return nil
+}
+
+// ReportPanic of a mockReporter does nothing and return nil for error.
+func (r *mockReporter) ReportPanic(v int64) error {
+	return nil
+}
+
+func makeEndpoints() *corev1.Endpoints {
+	return &corev1.Endpoints{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: testNamespace,
+			Name:      "revisionService",
+		},
+	}
+}
+
+func addIps(ep *corev1.Endpoints, ipCount int) *corev1.Endpoints {
+	epAddresses := []corev1.EndpointAddress{}
+	for i := 1; i <= ipCount; i++ {
+		ip := fmt.Sprintf("127.0.0.%v", i)
+		epAddresses = append(epAddresses, corev1.EndpointAddress{IP: ip})
+	}
+	ep.Subsets = []corev1.EndpointSubset{{
+		Addresses: epAddresses,
+	}}
+	return ep
+}
+
+func createEndpoints(ep *corev1.Endpoints) {
+	fakeClient.CoreV1().Endpoints(testNamespace).Create(ep)
+	endpointsInformer.Informer().GetIndexer().Add(ep)
+}
