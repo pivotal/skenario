2baec3c5a3360e72c9a7f31088782a4b443410ca
Jacques Chester <jchester@pivotal.io>
2019-03-25 18:17:56 -0400

    Begin adding Environment

---
+++ pkg/newsimulator/environment.go
@@ -0,0 +1,98 @@
+package newsimulator
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"k8s.io/client-go/tools/cache"
+)
+
+const (
+	OccursInPast    = "ScheduledToOccurInPast"
+	OccursAfterHalt = "ScheduledToOccurAfterHalt"
+)
+
+type Environment interface {
+	AddToSchedule(movement Movement) (added bool)
+	Run() (results []CompletedMovement, movements []IgnoredMovement, err error)
+}
+
+type CompletedMovement struct {
+	movement Movement
+}
+
+type IgnoredMovement struct {
+	reason   string
+	movement Movement
+}
+
+type environment struct {
+	simulationTime time.Time
+	haltTime       time.Time
+
+	futureMovements *cache.Heap
+	completed       []CompletedMovement
+	ignored         []IgnoredMovement
+}
+
+func (env *environment) AddToSchedule(movement Movement) (added bool) {
+	occursAfterCurrent := movement.OccursAt().After(env.simulationTime)
+	occursBeforeHalt := movement.OccursAt().Before(env.haltTime)
+	schedulable := occursAfterCurrent && occursBeforeHalt
+	if schedulable {
+		err := env.futureMovements.Add(movement)
+		if err != nil {
+			panic(fmt.Errorf("could not add '%#v' to future movements: %s", movement, err.Error()))
+		}
+	} else if !occursAfterCurrent {
+		env.ignored = append(env.ignored, IgnoredMovement{
+			reason:   OccursInPast,
+			movement: movement,
+		})
+	} else if !occursBeforeHalt {
+		env.ignored = append(env.ignored, IgnoredMovement{
+			reason:   OccursAfterHalt,
+			movement: movement,
+		})
+	}
+
+	return schedulable
+}
+
+func (env *environment) Run() (results []CompletedMovement, movements []IgnoredMovement, err error) {
+	// TODO: totally fake while tests get filled out
+	// TODO: as this won't preserve order
+	list := env.futureMovements.List()
+	for _, l := range list {
+		mv := l.(Movement)
+		env.completed = append(env.completed, CompletedMovement{movement: mv})
+	}
+
+	return env.completed, env.ignored, nil
+}
+
+func NewEnvironment(startAt time.Time, runFor time.Duration) Environment {
+	heap := cache.NewHeap(occursAtToKey, leftMovementIsEarlier)
+
+	return &environment{
+		simulationTime: startAt,
+		haltTime:       startAt.Add(runFor),
+
+		futureMovements: heap,
+		completed:       make([]CompletedMovement, 0),
+		ignored:         make([]IgnoredMovement, 0),
+	}
+}
+
+func occursAtToKey(movement interface{}) (key string, err error) {
+	mv := movement.(Movement)
+	return strconv.FormatInt(mv.OccursAt().UnixNano(), 10), nil
+}
+
+func leftMovementIsEarlier(left interface{}, right interface{}) bool {
+	l := left.(Movement)
+	r := right.(Movement)
+
+	return l.OccursAt().Before(r.OccursAt())
+}
---
+++ pkg/newsimulator/environment_test.go
@@ -0,0 +1,193 @@
+package newsimulator
+
+import (
+	"testing"
+	"time"
+
+	"github.com/sclevine/spec"
+	"github.com/sclevine/spec/report"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEnvironment(t *testing.T) {
+	spec.Run(t, "Environment spec", testEnvironment, spec.Report(report.Terminal{}))
+}
+
+func testEnvironment(t *testing.T, describe spec.G, it spec.S) {
+	var (
+		subject   Environment
+		movement  Movement
+		fromStock SourceStock
+		toStock   SinkStock
+		startTime time.Time
+	)
+
+	startTime = time.Unix(222222, 0)
+
+	it.Before(func() {
+		subject = NewEnvironment(startTime, 555555*time.Second)
+		assert.NotNil(t, subject)
+
+		fromStock = NewSourceStock("from stock", "test entity kind")
+		toStock = NewSinkStock("to stock", "test entity kind")
+	})
+
+	describe("AddToSchedule()", func() {
+		describe("the scheduled movement will occur during the simulation", func() {
+			it("returns true", func() {
+				movement = NewMovement(time.Unix(333333, 0), fromStock, toStock)
+				assert.True(t, subject.AddToSchedule(movement))
+			})
+		})
+
+		describe("the scheduled movement would occur after the simulation ends", func() {
+			it("returns false", func() {
+				movement = NewMovement(time.Unix(999999, 0), fromStock, toStock)
+				assert.False(t, subject.AddToSchedule(movement))
+			})
+		})
+
+		describe("the movement would occur at or before the current simulation time", func() {
+			it("returns false", func() {
+				movement = NewMovement(time.Unix(222222, 0), fromStock, toStock)
+				assert.False(t, subject.AddToSchedule(movement))
+
+				movement = NewMovement(time.Unix(111111, 0), fromStock, toStock)
+				assert.False(t, subject.AddToSchedule(movement))
+			})
+		})
+
+		describe.Pend("Schedule listeners", func() {
+			it("calls OnSchedule() on registered listeners", func() {
+
+			})
+		})
+	})
+
+	describe("Run()", func() {
+		// TODO: the tests below will probably require some manner of mockery and/or filling out stocks and movements
+
+		describe.Pend("taking the next movement from the schedule", func() {
+
+		})
+
+		describe.Pend("moving the Entity from the Source to the Sink", func() {
+
+		})
+
+		describe.Pend("Start-simulation movement", func() {
+
+		})
+
+		describe.Pend("Halt-simulation movement", func() {
+
+		})
+
+		describe("results", func() {
+			describe("completed movements", func() {
+				var first, second Movement
+				var completed []CompletedMovement
+
+				it.Before(func() {
+					var err error
+
+					first = NewMovement(time.Unix(333333, 0), fromStock, toStock)
+					second = NewMovement(time.Unix(444444, 0), fromStock, toStock)
+
+					subject.AddToSchedule(first)
+					subject.AddToSchedule(second)
+					completed, _, err = subject.Run()
+
+					assert.NoError(t, err)
+				})
+
+				it("contains the correct number of completed movements", func() {
+					assert.Len(t, completed, 2)
+				})
+
+				it("contains the completed movements", func() {
+					assert.Contains(t, completed, CompletedMovement{movement: first})
+					assert.Contains(t, completed, CompletedMovement{movement: second})
+				})
+			})
+
+			describe("ignored movements", func() {
+				var tooEarly, tooLate, goldilocks Movement
+				var ignored []IgnoredMovement
+
+				it.Before(func() {
+					var err error
+
+					tooEarly = NewMovement(time.Unix(111111, 0), fromStock, toStock)
+					goldilocks = NewMovement(time.Unix(333333, 0), fromStock, toStock)
+					tooLate = NewMovement(time.Unix(999999, 0), fromStock, toStock)
+
+					subject.AddToSchedule(tooEarly)
+					subject.AddToSchedule(goldilocks)
+					subject.AddToSchedule(tooLate)
+					_, ignored, err = subject.Run()
+
+					assert.NoError(t, err)
+				})
+
+				it("contains the correct number of ignored movements", func() {
+					assert.Len(t, ignored, 2)
+				})
+
+				it("contains movements that were scheduled in the past", func() {
+					assert.Contains(t, ignored, IgnoredMovement{reason: OccursInPast, movement: tooEarly})
+				})
+
+				it("contains movements that were scheduled after the halt", func() {
+					assert.Contains(t, ignored, IgnoredMovement{reason: OccursAfterHalt, movement: tooLate})
+				})
+
+				it("doesn't contain any events that were scheduled", func() {
+					assert.NotContains(t, ignored, IgnoredMovement{reason: OccursInPast, movement: goldilocks})
+					assert.NotContains(t, ignored, IgnoredMovement{reason: OccursAfterHalt, movement: goldilocks})
+				})
+			})
+		})
+
+		describe.Pend("AddScheduleListener()", func() {
+			it("adds a registered listener", func() {
+
+			})
+		})
+
+		describe("helper funcs", func() {
+			describe("occursAtToKey()", func() {
+				it.Before(func() {
+					movement = NewMovement(time.Unix(0, 111000111), fromStock, toStock)
+				})
+
+				it("returns the OccursAt() as a string", func() {
+					key, err := occursAtToKey(movement)
+					assert.NoError(t, err)
+					assert.Equal(t, "111000111", key)
+				})
+			})
+
+			describe("leftMovementIsEarlier()", func() {
+				var earlier, later Movement
+
+				it.Before(func() {
+					earlier = NewMovement(time.Unix(111, 0), fromStock, toStock)
+					later = NewMovement(time.Unix(999, 0), fromStock, toStock)
+				})
+
+				describe("when the first argument is earlier", func() {
+					it("returns true", func() {
+						assert.True(t, leftMovementIsEarlier(earlier, later))
+					})
+				})
+
+				describe("when the second argument is earlier", func() {
+					it("returns false", func() {
+						assert.False(t, leftMovementIsEarlier(later, earlier))
+					})
+				})
+			})
+		})
+	})
+}
