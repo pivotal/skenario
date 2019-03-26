0e33470b7eae71b6f27a04f98942cbdf672bf60a
Jacques Chester <jacques@chester.id.au>
2019-03-26 19:49:46 -0400

    WIP start adding new autoscaler model
    As in previous commits, using a new package as a temporary
    step until I don't need old ones at easy reference.

---
+++ pkg/newmodel/autoscaler.go
@@ -0,0 +1,63 @@
+package newmodel
+
+import (
+	"time"
+
+	"knative-simulator/pkg/newsimulator"
+
+	"github.com/knative/serving/pkg/autoscaler"
+)
+
+type KnativeAutoscaler interface {
+}
+
+type knativeAutoscaler struct {
+	env        newsimulator.Environment
+	tickTock   *tickTock
+	autoscaler *autoscaler.Autoscaler
+}
+
+func NewKnativeAutoscaler(env newsimulator.Environment, startAt time.Time) KnativeAutoscaler {
+	kas := &knativeAutoscaler{
+		env:        env,
+		tickTock:   &tickTock{},
+		autoscaler: nil,
+	}
+
+	firstCalculation := newsimulator.NewMovement(
+		startAt.Add(2*time.Second),
+		kas.tickTock,
+		kas.tickTock,
+		"Autoscaler calculating",
+	)
+
+	env.AddToSchedule(firstCalculation)
+
+	return kas
+}
+
+type tickTock struct {
+	asEntity newsimulator.Entity
+}
+
+func (tt *tickTock) Name() newsimulator.StockName {
+	return "KnativeAutoscaler Stock"
+}
+
+func (tt *tickTock) KindStocked() newsimulator.EntityKind {
+	return newsimulator.EntityKind("KnativeAutoscaler")
+}
+
+func (tt *tickTock) Count() uint64 {
+	return 1
+}
+
+func (tt *tickTock) Remove() newsimulator.Entity {
+	return tt.asEntity
+}
+
+func (tt *tickTock) Add(entity newsimulator.Entity) error {
+	tt.asEntity = entity
+
+	return nil
+}
---
+++ pkg/newmodel/autoscaler_test.go
@@ -0,0 +1,124 @@
+package newmodel
+
+import (
+	"testing"
+	"time"
+
+	"github.com/sclevine/spec"
+	"github.com/sclevine/spec/report"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"knative-simulator/pkg/newsimulator"
+)
+
+func TestAutoscaler(t *testing.T) {
+	spec.Run(t, "KnativeAutoscaler model", testAutoscaler, spec.Report(report.Terminal{}))
+}
+
+type mockEnvironment struct {
+	mock.Mock
+}
+
+func (me *mockEnvironment) AddToSchedule(movement newsimulator.Movement) (added bool) {
+	me.Called(movement)
+	return true
+}
+
+func (me *mockEnvironment) Run() (completed []newsimulator.CompletedMovement, ignored []newsimulator.IgnoredMovement, err error) {
+	return nil, nil, nil
+}
+
+func testAutoscaler(t *testing.T, describe spec.G, it spec.S) {
+	// var subject KnativeAutoscaler
+	var mockEnv *mockEnvironment
+	startAt := time.Unix(0, 0)
+	// runFor := 1 * time.Minute
+
+	it.Before(func() {
+		mockEnv = new(mockEnvironment)
+		mockEnv.On("AddToSchedule", mock.Anything).Return(true) // TODO: I'd rather not use mock.Anything
+	})
+
+	describe("NewKnativeAutoscaler()", func() {
+		it.Before(func() {
+			_ = NewKnativeAutoscaler(mockEnv, startAt)
+		})
+
+		it("schedules a first calculation", func() {
+			mockEnv.AssertExpectations(t)
+		})
+	})
+
+	describe("tickTock stock", func() {
+		it.Before(func() {
+			_ = NewKnativeAutoscaler(mockEnv, startAt)
+		})
+		ttStock := &tickTock{}
+
+		describe("Name()", func() {
+			it("is called 'KnativeAutoscaler Stock'", func() {
+				assert.Equal(t, ttStock.Name(), newsimulator.StockName("KnativeAutoscaler Stock"))
+			})
+		})
+
+		describe("KindStocked()", func() {
+			it("accepts Autoscalers", func() {
+				assert.Equal(t, ttStock.KindStocked(), newsimulator.EntityKind("KnativeAutoscaler"))
+			})
+		})
+
+		describe("Count()", func() {
+			it("always has 1 entity stocked", func() {
+				assert.Equal(t, ttStock.Count(), uint64(1))
+
+				err := ttStock.Add(newsimulator.NewEntity("test entity", newsimulator.EntityKind("KnativeAutoscaler")))
+				assert.NoError(t, err)
+
+				assert.Equal(t, ttStock.Count(), uint64(1))
+			})
+		})
+
+		describe("Remove()", func() {
+			it("gives back the one KnativeAutoscaler", func() {
+				entity := newsimulator.NewEntity("test entity", newsimulator.EntityKind("KnativeAutoscaler"))
+				err := ttStock.Add(entity)
+				assert.NoError(t, err)
+
+				assert.Equal(t, ttStock.Remove(), entity)
+			})
+		})
+
+		describe("Add()", func() {
+			it("adds the entity if it's not already set", func() {
+				assert.Nil(t, ttStock.asEntity)
+
+				entity := newsimulator.NewEntity("test entity", newsimulator.EntityKind("KnativeAutoscaler"))
+				err := ttStock.Add(entity)
+				assert.NoError(t, err)
+
+				assert.Equal(t, ttStock.asEntity, entity)
+			})
+
+			it("schedules an event for the next calculation", func() {
+				mockEnv.On("AddToSchedule", mock.Anything).Return(true) // TODO: I'd rather not use mock.Anything
+
+				err := ttStock.Add(ttStock.asEntity)
+				assert.NoError(t, err)
+
+				mockEnv.AssertCalled(t, "AddToSchedule", newsimulator.NewMovement(
+					startAt.Add(2*time.Second),
+					ttStock,
+					ttStock,
+					"Autoscaler calculating",
+				))
+
+				mockEnv.AssertNumberOfCalls(t, "AddToSchedule", 2)
+			})
+
+			it.Pend("triggers the autoscaler calculation", func() {
+
+			})
+		})
+	})
+}
