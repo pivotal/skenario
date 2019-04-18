cf1b891df4b3e38e7a6d8119000c3220f93f002f
Jacques Chester <jchester@pivotal.io>
2019-04-18 16:33:38 -0700

    Introduce ReplicasDesired stock
    [pivotal/skenario#5]

---
+++ pkg/model/replicas_desired.go
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2019-Present Pivotal Software, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms
+ * of the Apache License, Version 2.0 (the "License”); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ */
+
+package model
+
+import (
+	"time"
+
+	"skenario/pkg/simulator"
+)
+
+type ReplicasConfig struct {
+	LaunchDelay    time.Duration
+	TerminateDelay time.Duration
+}
+
+type ReplicasDesiredStock interface {
+	simulator.ThroughStock
+}
+
+type replicasDesiredStock struct {
+	env                simulator.Environment
+	config             ReplicasConfig
+	delegate           simulator.ThroughStock
+	replicaSource      ReplicaSource
+	replicasLaunching  simulator.ThroughStock
+	replicasActive     simulator.ThroughStock
+	replicasTerminated simulator.SinkStock
+	launchingCount     uint64
+}
+
+func (rds *replicasDesiredStock) Name() simulator.StockName {
+	return rds.delegate.Name()
+}
+
+func (rds *replicasDesiredStock) KindStocked() simulator.EntityKind {
+	return rds.delegate.KindStocked()
+}
+
+func (rds *replicasDesiredStock) Count() uint64 {
+	return rds.delegate.Count()
+}
+
+func (rds *replicasDesiredStock) EntitiesInStock() []*simulator.Entity {
+	return rds.delegate.EntitiesInStock()
+}
+
+func (rds *replicasDesiredStock) Remove() simulator.Entity {
+	err := rds.delegate.Remove()
+	if err != nil {
+		return err
+	}
+
+	nextTerminate := rds.env.CurrentMovementTime().Add(rds.config.TerminateDelay)
+	if rds.replicasLaunching.Count() > 0 {
+		rds.env.AddToSchedule(simulator.NewMovement(
+			"terminate_launch",
+			nextTerminate,
+			rds.replicasLaunching,
+			rds.replicasTerminated,
+		))
+	} else {
+		rds.env.AddToSchedule(simulator.NewMovement(
+			"terminate_active",
+			nextTerminate,
+			rds.replicasActive,
+			rds.replicasTerminated,
+		))
+	}
+
+	return nil
+}
+
+func (rds *replicasDesiredStock) Add(entity simulator.Entity) error {
+	err := rds.delegate.Add(entity)
+	if err != nil {
+		return err
+	}
+
+	rds.env.AddToSchedule(simulator.NewMovement(
+		"begin_launch",
+		rds.env.CurrentMovementTime().Add(1*time.Nanosecond),
+		rds.replicaSource,
+		rds.replicasLaunching,
+	))
+
+	rds.env.AddToSchedule(simulator.NewMovement(
+		"finish_launching",
+		rds.env.CurrentMovementTime().Add(rds.config.LaunchDelay),
+		rds.replicasLaunching,
+		rds.replicasActive,
+	))
+
+	return nil
+}
+
+func NewReplicasDesiredStock(env simulator.Environment, config ReplicasConfig, replicaSource ReplicaSource, replicasLaunching, replicasActive simulator.ThroughStock, replicasTerminated simulator.SinkStock) ReplicasDesiredStock {
+	return &replicasDesiredStock{
+		env:                env,
+		config:             config,
+		delegate:           simulator.NewThroughStock("ReplicasDesired", "Desired"),
+		replicaSource:      replicaSource,
+		replicasLaunching:  replicasLaunching,
+		replicasActive:     replicasActive,
+		replicasTerminated: replicasTerminated,
+	}
+}
---
+++ pkg/model/replicas_desired_test.go
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2019-Present Pivotal Software, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms
+ * of the Apache License, Version 2.0 (the "License”); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ */
+
+package model
+
+import (
+	"testing"
+	"time"
+
+	"github.com/sclevine/spec"
+	"github.com/sclevine/spec/report"
+	"github.com/stretchr/testify/assert"
+
+	"skenario/pkg/model/fakes"
+	"skenario/pkg/simulator"
+)
+
+func TestReplicasDesired(t *testing.T) {
+	spec.Run(t, "ReplicasDesired stock", testReplicasDesired, spec.Report(report.Terminal{}))
+}
+
+func testReplicasDesired(t *testing.T, describe spec.G, it spec.S) {
+	var subject ReplicasDesiredStock
+	var rawSubject *replicasDesiredStock
+	var config ReplicasConfig
+	var replicaSource ReplicaSource
+	var replicasLaunching, replicasActive simulator.ThroughStock
+	var replicasTerminated simulator.SinkStock
+	var envFake *fakes.FakeEnvironment
+
+	it.Before(func() {
+		replicasLaunching = simulator.NewThroughStock("ReplicasLaunching", "Replica")
+		replicasActive = simulator.NewThroughStock("ReplicasActive", "Replica")
+		replicasTerminated = simulator.NewThroughStock("ReplicasTerminated", "Replica")
+		replicaSource = NewReplicaSource(envFake, nil, nil)
+		config = ReplicasConfig{LaunchDelay: 1 * time.Second, TerminateDelay: 1 * time.Nanosecond}
+		envFake = new(fakes.FakeEnvironment)
+		envFake.Movements = make([]simulator.Movement, 0)
+
+		subject = NewReplicasDesiredStock(envFake, config, replicaSource, replicasLaunching, replicasActive, replicasTerminated)
+		rawSubject = subject.(*replicasDesiredStock)
+	})
+
+	describe("Name()", func() {
+		it("calls itself ReplicasDesired", func() {
+			assert.Equal(t, simulator.StockName("ReplicasDesired"), subject.Name())
+		})
+	})
+
+	describe("KindStocked()", func() {
+		it("stocks Desireds", func() {
+			assert.Equal(t, simulator.EntityKind("Desired"), subject.KindStocked())
+		})
+	})
+
+	describe("Count()", func() {
+		it("gives the count of added and removed", func() {
+			assert.Equal(t, uint64(0), subject.Count())
+
+			subject.Add(simulator.NewEntity("desired-1", "Desired"))
+			assert.Equal(t, uint64(1), subject.Count())
+
+			subject.Remove()
+			assert.Equal(t, uint64(0), subject.Count())
+		})
+	})
+
+	describe("EntitiesInStock()", func() {
+		it("returns an array of Desired", func() {
+			ent := simulator.NewEntity("desired-1", "Desired")
+			subject.Add(ent)
+			assert.Equal(t, []*simulator.Entity{&ent}, subject.EntitiesInStock())
+		})
+	})
+
+	describe("Add()", func() {
+		it.Before(func() {
+			subject.Add(simulator.NewEntity("add-1", "Desired"))
+		})
+
+		it("schedules movements of new entities from ReplicaSource to ReplicasLaunching", func() {
+			assert.Equal(t, simulator.MovementKind("begin_launch"), envFake.Movements[0].Kind())
+		})
+
+		it("schedules movements of new entities from ReplicasLaunching to ReplicasActive", func() {
+			assert.Equal(t, simulator.MovementKind("finish_launching"), envFake.Movements[1].Kind())
+		})
+	})
+
+	describe("Remove()", func() {
+		describe("there are launching replicas but no active replicas", func() {
+			it.Before(func() {
+				err := rawSubject.replicasLaunching.Add(simulator.NewEntity("already launching", simulator.EntityKind("Replica")))
+				assert.NoError(t, err)
+
+				subject.Remove()
+			})
+
+			it("schedules movements from ReplicasLaunching to ReplicasTerminating", func() {
+				assert.Len(t, envFake.Movements, 1)
+				assert.Equal(t, simulator.MovementKind("terminate_launch"), envFake.Movements[0].Kind())
+			})
+		})
+
+		describe("there are active replicas but no launching replicas", func() {
+			it.Before(func() {
+				newReplica := NewReplicaEntity(envFake, nil, nil, "1.2.1.2")
+				err := rawSubject.replicasActive.Add(newReplica)
+				assert.NoError(t, err)
+
+				subject.Remove()
+			})
+
+			it("schedules movements from ReplicasActive to ReplicasTerminating", func() {
+				assert.Len(t, envFake.Movements, 1)
+				assert.Equal(t, simulator.MovementKind("terminate_active"), envFake.Movements[0].Kind())
+			})
+		})
+
+		//TODO: this won't work properly without batch movement: https://github.com/pivotal/skenario/issues/7
+		describe.Pend("there is a mix of active and launching replicas", func() {
+			it.Before(func() {
+				newReplica := NewReplicaEntity(envFake, nil, nil, "3.4.3.4")
+				err := rawSubject.replicasActive.Add(newReplica)
+				assert.NoError(t, err)
+				err = rawSubject.replicasLaunching.Add(simulator.NewEntity("already launching", simulator.EntityKind("Replica")))
+				assert.NoError(t, err)
+
+				subject.Remove()
+				subject.Remove()
+			})
+
+			it("schedules movements from ReplicasActive to ReplicasTerminating", func() {
+				assert.Len(t, envFake.Movements, 2)
+				assert.Equal(t, "terminate_launch", string(envFake.Movements[0].Kind()))
+				assert.Equal(t, "terminate_active", string(envFake.Movements[1].Kind()))
+			})
+		})
+
+		describe.Pend("there are no active or launching replicas", func() {
+			// can this actually happen?
+		})
+	})
+}
